0be5f30255883bc0fff7be4b915e0499e3a23d1a
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-05-04 15:06:54 +0700

    Part02Mono done
    - Mono.empty()
    - Mono.never()
    - Mono.just()
    - Mono.error()

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -13,29 +13,37 @@ public class Part02Mono {
 //========================================================================================
 
 	// TODO Return an empty Mono
+	// empty()
+	// Create a Mono that completes without emitting any item.
 	Mono<String> emptyMono() {
-		return null;
+		return Mono.empty();
 	}
 
 //========================================================================================
 
 	// TODO Return a Mono that never emits any signal
+	// never()
+	// Return a Mono that will never signal any data, error or completion signal, essentially running indefinitely.
 	Mono<String> monoWithNoSignal() {
-		return null;
+		return Mono.never();
 	}
 
 //========================================================================================
 
 	// TODO Return a Mono that contains a "foo" value
+	// just(T data)
+	// Create a new Mono that emits the specified item, which is captured at instantiation time
 	Mono<String> fooMono() {
-		return null;
+		return Mono.just("foo");
 	}
 
 //========================================================================================
 
 	// TODO Create a Mono that emits an IllegalStateException
+	// error(Throwable error)
+	// Create a Mono that terminates with the specified error immediately after being subscribed to.
 	Mono<String> errorMono() {
-		return null;
+		return Mono.error(new IllegalStateException());
 	}
 
 }
