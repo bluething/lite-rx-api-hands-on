1114b65c11a2c29446e31e49cf95b5b60439e1f7
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-05-04 14:57:59 +0700

    Part01Flux done
    - Flux.empty()
    - Flux.just()
    - Flux.fromArray()
    - Flux.interval()
    - Flux.just()

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,5 +1,7 @@
 package io.pivotal.literx;
 
+import java.time.Duration;
+
 import reactor.core.publisher.Flux;
 
 /**
@@ -13,36 +15,49 @@ public class Part01Flux {
 //========================================================================================
 
 	// TODO Return an empty Flux
+	// Flux.empty()
+	// Create a Flux that completes without emitting any item.
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+	// just(T... data)
+	// Create a Flux that emits the provided elements and then completes.
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo", "bar");
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+	// fromArray(T[] array)
+	// Create a Flux that emits the items contained in the provided array. 
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		return Flux.fromArray(new String[]{"foo", "bar"});
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux that emits an IllegalStateException
+	// error(Throwable error)
+	// Create a Flux that terminates with the specified error immediately after being subscribed to.
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(new IllegalStateException());
 	}
 
 //========================================================================================
 
-		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+	// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+	// interval(Duration period)
+	// Create a Flux that emits long values starting with 0 and incrementing at specified time intervals on the global timer.
+	// take(long n)
+	// Take only the first N values from this Flux, if available.
 	Flux<Long> counter() {
-		return null;
+		return Flux.interval(Duration.ofMillis(100))
+				.take(10);
 	}
 
 }
