c95d39ba266b502da41351fcfdfabb9f64073549
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-05-07 11:08:52 +0700

    Part10ReactiveToBlocking done
    - Mono.block()
    - Flux.toIterable()

--- src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
+++ src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
@@ -14,15 +14,21 @@ public class Part10ReactiveToBlocking {
 //========================================================================================
 
 	// TODO Return the user contained in that Mono
+	// Mono.block()
+	// Subscribe to this Mono and block indefinitely until a next signal is received.
+	// Returns that value, or null if the Mono completes empty.
+	// In case the Mono errors, the original exception is thrown (wrapped in a RuntimeException if it was a checked exception).
 	User monoToValue(Mono<User> mono) {
-		return null;
+		return mono.block();
 	}
 
 //========================================================================================
 
 	// TODO Return the users contained in that Flux
+	// Flux.toIterable()
+	// Transform this Flux into a lazy Iterable blocking on Iterator.next() calls.
 	Iterable<User> fluxToValues(Flux<User> flux) {
-		return null;
+		return flux.toIterable();
 	}
 
 }
