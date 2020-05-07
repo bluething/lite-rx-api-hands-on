e2ff113b60ba40540b9995157c0f3c3ccff6c532
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-05-07 11:19:27 +0700

    Part11BlockingToReactive done
    - Flux.defer
    - Flux.subscribeOn
    - Flux.publishOn

--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -26,15 +26,29 @@ public class Part11BlockingToReactive {
 //========================================================================================
 
 	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
+	// Flux.defer(Supplier<? extends Publisher<User>> supplier)
+	// Lazily supply a Publisher every time a Subscription is made on the resulting Flux, 
+	// so the actual source instantiation is deferred until each subscribe and the Supplier can create a subscriber-specific instance.
+	// If the supplier doesn't generate a new instance however, this operator will effectively behave like from(Publisher).
+	// Flux.subscribeOn(Scheduler scheduler)
+	// Run subscribe, onSubscribe and request on a specified Scheduler's Scheduler.Worker.
+	// As such, placing this operator anywhere in the chain will also impact the execution context of onNext/onError/onComplete signals from the beginning of the chain up to the next occurrence of a publishOn.
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
-		return null;
+		return Flux.defer(
+				() -> Flux.fromIterable(repository.findAll()))
+				.subscribeOn(Schedulers.elastic());
 	}
 
 //========================================================================================
 
 	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
+	// Flux.publishOn(Scheduler scheduler)
+	// Run onNext, onComplete and onError on a supplied Scheduler Worker.
+	// This operator influences the threading context where the rest of the operators in the chain below it will execute, up to a new occurrence of publishOn.
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		return flux
+				.publishOn(Schedulers.elastic())
+				.doOnNext(repository::save).then();
 	}
 
 }
